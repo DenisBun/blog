1165d276d3754083a12b1eb092eac0223ef8cb00
Denis B. <denis.bunchenko@nextaim.de>
2026-08-04 19:51:24 +0200

    add a bunch of integrations;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro'
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()])]
+  site: "https://denisbunchenko.com/",
+  integrations: [
+    react(),
+    markdoc(),
+    ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
+    sitemap(),
+    vue(),
+    svelte(),
+    icon(),
+    partytown(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
